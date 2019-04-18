b787c6d733156e6c5b4f3671a0132f81db39f425
Eric Lin <ericyilin8@gmail.com>
2019-04-18 17:09:43 -0400

    new

--- javascript/music.js
+++ javascript/music.js
@@ -1,13 +1,16 @@
 var search;
 
 $('#searchButton').on('click', function () {
+    $('#results').empty();
+    $('#results').text('Searching...');
     search = $('#songsearch').val().trim();
-    $('#results').text('Sorry there was an error!');
+
+
     if (search == '') {
         $('#results').text('Please enter a song name and/or artist.');
 
     }
-    console.log(search);
+    //console.log(search);
     //var queryurl = 'https://api.musixmatch.com/ws/1.1/track.search?q=' + search + '&apikey=97e346e9b545a7dab54fae058f0f85cc';
     //var queryurl = 'https://api.musixmatch.com/ws/1.1/chart.artists.get?page=1&page_size=3&country=it';
     //var queryurl = 'https://api.musixmatch.com/ws/1.1/matcher.track.get?q_track=eminem' + '&apikey=97e346e9b545a7dab54fae058f0f85cc';
@@ -18,7 +21,7 @@ $('#searchButton').on('click', function () {
         method: 'GET'
     })
         .then(function (response) {
-            console.log(response);
+            //console.log(response);
             $('#results').empty();
             response.search.data.tracks.forEach(function (currentValue) {
 
@@ -26,9 +29,15 @@ $('#searchButton').on('click', function () {
                 var song = currentValue.name;
                 var album = currentValue.albumName;
                 var songurl = currentValue.previewURL;
-                $('#results').append('<div class="selectsong" data-song="' + song + '" data-artist="' + artist + '" data-songurl="'+songurl+'">' + song + ' - ' + artist + ', Album: ' + album + '</div>');
+                $('#results').append('<div class="selectsong" data-song="' + song + '" data-artist="' + artist + '" data-songurl="' + songurl + '">' + song + ' - ' + artist + ', Album: ' + album + '</div>');
 
             })
+        }).fail(function (response) {
+            if (search == '') {
+                $('#results').text('Please enter a song name and/or artist.');
+
+            } else { $('#results').text('Sorry there was an error!'); }
+
         })
 
     /*var searchAlbums = function (query) {
@@ -76,25 +85,42 @@ $('#searchButton').on('click', function () {
     });*/
 })
 
+$('#mainsong').attr('data-song', 'none');
+
 $('body').on('click', '.selectsong', function () {
     $('#songTitle').text(this.dataset.song);
     //console.log(this.dataset.song);
     $('#artist').text(this.dataset.artist);
-    console.log(this.dataset.songurl);
+    //console.log(this.dataset.songurl);
+
+
     $('#mainsong').attr('data-song', this.dataset.songurl);
+    $('#mainsong span').attr('class', 'glyphicon glyphicon-play-circle');
+
+    if ($('#mainsong').attr('data-playing') == 'true') {
+        $('#mainsong').attr('data-playing', 'false');
+        changeSong(this.dataset.songurl);
+    }
+
+
+
     var queryurl = 'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=' + this.dataset.song + '&q_artist=' + this.dataset.artist + '&apikey=97e346e9b545a7dab54fae058f0f85cc';
     $.ajax({
         url: queryurl,
         method: 'GET'
     }).then(function (response) {
-        console.log(response);
-        var lyrics = response.message.body.lyrics.lyrics_body;
-        lyrics = lyrics.replace(/\r?\n/g, "<br />");
-        console.log(lyrics);
-        if (lyrics != '') {
+
+        if ('lyrics' in response.message.body) {
+            //console.log(response);
+            var lyrics = response.message.body.lyrics.lyrics_body;
+            lyrics = lyrics.replace(/\r?\n/g, "<br />");
+        }
+
+        //console.log(lyrics);
+        if (lyrics != undefined & lyrics != '') {
             $('#lyrics').html(lyrics);
         }
-        else{
+        else {
             $('#lyrics').html('Sorry no lyrics were found!');
         }
 
@@ -103,43 +129,77 @@ $('body').on('click', '.selectsong', function () {
 //&#8629 the return character
 // \r\n characters
 
-$('.play').on('click', function () {
-
-})
-
 $('.heart').on('click', function () {
 
 })
 
 //load top 5 songs of US
 $.ajax({
-    url: 'https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=us&apikey=97e346e9b545a7dab54fae058f0f85cc',
+    url: 'https://api.napster.com/v2.2/tracks/top?apikey=YTkxZTRhNzAtODdlNy00ZjMzLTg0MWItOTc0NmZmNjU4Yzk4&limit=5',
     method: 'GET'
-}).then(function(response){
-    console.log(response);
-    response.message.body.track_list.forEach(function(currentValue, index){
-        var artist = currentValue.track.artist_name;
-        var song = currentValue.track.track_name;
-        console.log(song);
-        $('#song'+(index+1)).text(song);
-        $('#artist'+(index+1)).text(artist);
+}).then(function (response) {
+    //console.log(response);
+    response.tracks.forEach(function (currentValue, index) {
+        var artist = currentValue.artistName;
+        var song = currentValue.name;
+        var preview = currentValue.previewURL;
+        //console.log(song);
+        $('#song' + (index + 1)).text(song);
+        $('#artist' + (index + 1)).text(artist);
+
+        $('#songBtn' + (index + 1)).attr('data-song', preview);
 
     });
 })
 
-function playSong(songurl){
+function changeSong(songurl) {
     $('#player').attr('src', songurl);
-    $('#player')[0].play();
+
 }
 
-$('.playbtn').on('click', function(){
-    playSong(this.dataset.song);
+$('#player').on('ended', function(){
+    $('.playing span').attr('class', 'glyphicon glyphicon-repeat');
+    $('.playing').attr('data-playing', false);
+    $('.playing').removeClass('playing');
 })
+
+$('.play').attr('data-playing', false);
+
+$('.play').on('click', function () {
+
+    if (this.dataset.song != 'none') {
+
+        if (this.dataset.playing == 'false') { //for some reason need to use string boolean not just boolean even with ==
+            $('.play').attr('data-playing', false);
+            $('.play span').attr('class', 'glyphicon glyphicon-play-circle');
+            $('.play').removeClass('playing');
+
+            if ($('#player').attr('src') != this.dataset.song) {
+                changeSong(this.dataset.song);
+            }
+
+            $(this).attr('data-playing', true);   
+            $(this).children('span').attr('class', 'glyphicon glyphicon-pause');
+            $(this).addClass('playing');
+            $('#player')[0].play();
+
+        }
+        else {
+            $(this).attr('data-playing', false);
+            $('.play').removeClass('playing');
+            $('#player')[0].pause();
+            $(this).children('span').attr('class', 'glyphicon glyphicon-play');
+        }
+    }
+
+
+
+});
 
 //search using napster api query type track /done
 //display 10 results and user selects one /done
 //use musicxmatch matcher.lyrics.get to get the lyrics /done
-// use mxm chart.artists.get to get the top 5 songs
+// use mxm chart.artists.get to get the top 5 songs -> use napster because napster also provides a previewURL
 //save username, password, and favorites to firebase
 
 
